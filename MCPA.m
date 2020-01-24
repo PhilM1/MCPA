a64bc6cd0f203ccb0016f78ea7f9d57b75e1c3b0
Phil <pm993@hotmail.com>
2020-01-24 13:51:46 -0500

    Added 3 plots, position, velocity and drift velocity. 3 particles (5% scatter, 5%scatter with 50% reflection, 25% scatter with 100% reflection)

--- MCPA.m
+++ MCPA.m
@@ -1,30 +1,72 @@
 %MCPA - Philippe Masson
 
-e_pos = 0;
-e_vel = 0;
+e_pos = [];
+e_pos(1:3) = 0;
+e_vel = [];
+e_vel(1:3) = 0;
 e_accel = 0;
 force_accel = 1;
 timestep = 0.1;
 currTime = 0;
+e_driftVel = [];
+e_driftVel(1:3) = 0;
+sumVel = [];
+sumVel(1:3) = 0;
+tiledlayout(3,1);
+set(gcf, 'Position', get(0, 'Screensize'));
 
-tiledlayout(2,1);
+e_oldVel = [];
+e_oldVel(1:3) = 0;
 
 while(1)
     currTime = currTime+timestep;
     e_accel = force_accel;
     e_oldVel = e_vel;
-    e_vel = e_vel + e_accel * timestep;
+    if(rand <= 0.05) %scatter e1
+        e_vel(1) = (-e_vel(1)*0) + e_accel * timestep;
+    else
+        e_vel(1) = e_vel(1) + e_accel * timestep;
+    end
+    
+    if(rand <= 0.05) %scatter e2 with 50% transfer
+        e_vel(2) = (-e_vel(2)*0.5) + e_accel * timestep;
+    else
+        e_vel(2) = e_vel(2) + e_accel * timestep;
+    end
+    
+    if(rand <= 0.25) %scatter e3 0% transfer
+        e_vel(3) = (-e_vel(3)*1) + e_accel * timestep;
+    else
+        e_vel(3) = e_vel(3) + e_accel * timestep;
+    end
+    
     e_pos = e_pos + e_vel * timestep;
+    
+    sumVel = sumVel + e_vel;
+    e_oldDrift = e_driftVel;
+    e_driftVel = sumVel ./ (currTime/timestep);
     
     nexttile(1);
     hold on;
-    scatter(currTime,e_pos, 'b');
+    scatter(e_pos(1),1, 'b');
+    scatter(e_pos(2),2, 'g');
+    scatter(e_pos(3),3, 'r');
     title(['Position of Electron, time:' num2str(currTime)]);
     
     nexttile(2);
     hold on;
-    plot([currTime - timestep, currTime], [e_oldVel, e_vel], 'b');
+    plot([currTime - timestep, currTime], [e_oldVel(1), e_vel(1)], 'b');
+    plot([currTime - timestep, currTime], [e_oldVel(2), e_vel(2)], 'g');
+    plot([currTime - timestep, currTime], [e_oldVel(3), e_vel(3)], 'r');
+    title('Velocity of Electron');
+    
+    nexttile(3);
+    hold on;
+    plot([currTime - timestep, currTime], [e_oldDrift(1), e_driftVel(1)], 'b');
+    plot([currTime - timestep, currTime], [e_oldDrift(2), e_driftVel(2)], 'g');
+    plot([currTime - timestep, currTime], [e_oldDrift(3), e_driftVel(3)], 'r');
     title('Velocity of Electron');
 
-    pause(0.0001);
+    
+    pause(0.001);
 end
